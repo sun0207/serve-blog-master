c3f8faa8393969e20f9274f3c9f68f3db7c9bb38
wangcp <594906917@qq.com>
2020-06-30 15:23:04 +0800

    删除第三方配置

--- util/qq.config.js
+++ util/qq.config.js
@@ -3,8 +3,8 @@ module.exports = {
     'access_token_path': 'https://graph.qq.com/oauth2.0/token',
     'openid_path': 'https://graph.qq.com/oauth2.0/me',
     'user_info_path':'https://graph.qq.com/user/get_user_info',
-    'redirect_uri':'http://api.qianyaru.cn/githubLogin',
-    'client_id': '101882203',
-    'client_secret': '40b14d0a55e194694e6db2c89e5af000',
+    'redirect_uri':'http://api.qianyaru.cn/githubLogin', // 登录回调
+    'client_id': '', // qq互联应用appid
+    'client_secret': '', // qq互联应用APP Key
     'scope': ['get_user_info','get_vip_info','get_vip_rich_info'],
 };
--- util/webscoket.js
+++ util/webscoket.js
@@ -3,18 +3,18 @@ const http = require('http');
 const https = require('https');
 const fs = require('fs');
 const consola = require('consola');
-const options = {
-    key:fs.readFileSync(__dirname+'/Nginx/2_www.qianyaru.cn.key'),
-    cert:fs.readFileSync(__dirname+'/Nginx/1_www.qianyaru.cn_bundle.crt'),
-}
-const server = https.createServer(options,function(request, response) {
+//const options = {
+//    key:fs.readFileSync(__dirname+'/Nginx/xxx.key'),
+//   cert:fs.readFileSync(__dirname+'/Nginx/xxx.crt'),
+//}
+//const server = https.createServer(options,function(request, response) {
+//    response.writeHead(404);
+//    response.end();
+//});
+var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-// var server = http.createServer(function(request, response) {
-//     response.writeHead(404);
-//     response.end();
-// });
 server.listen(3355, function() {
     consola.success('\u001b[32m webscoket服务已建立;端口号:3355 \u001b[0m');
 });
